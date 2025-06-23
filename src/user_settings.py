c6827830f6213a7ff492bd264f757e989fbe3c49
Demolitionist <162406990+Demolitionist@users.noreply.github.com>
2025-06-23 12:01:55 +0800

    Update user_settings.py
    update subs

--- src/user_settings.py
+++ src/user_settings.py
@@ -11,25 +11,93 @@ SOURCE_URLS = [
 #    "https://raw.githubusercontent.com/10ium/ScrapeAndCategorize/refs/heads/main/output_configs/WireGuard.txt",
     "https://raw.githubusercontent.com/10ium/base64-encoder/main/encoded/ndsphonemy_hys-tuic.txt",
     "https://raw.githubusercontent.com/Leon406/SubCrawler/refs/heads/main/sub/share/hysteria2",
-    "https://t.me/s/v2rayfree",
-    "https://t.me/s/FreeV2rays",
-    "https://t.me/s/v2ray_free_conf",
-    "https://t.me/s/PrivateVPNs",
-    "https://t.me/s/IP_CF_Config",
-    "https://t.me/s/prrofile_purple",
-    "https://t.me/s/meli_proxyy",
-    "https://t.me/s/DirectVPN",
-    "https://t.me/s/VmessProtocol",
-    "https://t.me/s/vpnfail_vless",
-    "https://t.me/s/DailyV2RY",
-    "https://t.me/s/shadowproxy66",
-    "https://t.me/s/moftconfig",
-    "https://t.me/s/ConfigsHUB2",
+    "https://raw.githubusercontent.com/soroushmirzaei/telegram-configs-collector/main/channels/protocols/hysteria",
+    "https://raw.githubusercontent.com/Surfboardv2ray/Proxy-sorter/refs/heads/main/custom/udp.txt",
+    "https://raw.githubusercontent.com/Mosifree/-FREE2CONFIG/refs/heads/main/T,H",
+    "https://github.com/Mosifree/-FREE2CONFIG/blob/main/Movaghat",
+    "https://raw.githubusercontent.com/miladnataj6/nontlsvstls/refs/heads/main/pooriared.txt",
+    "https://raw.githubusercontent.com/pooriaredorg/POORIARED/refs/heads/main/POORIARED.txt",
+    "https://raw.githubusercontent.com/pooriaredorg/POORIA--REDFREE8/refs/heads/main/POORIA--REDFREE8.txt#POORIA--REDFREE8",
+    # "https://raw.githubusercontent.com/pooriaredorg/POORIAREDORG.pages/refs/heads/main/POORIAREDORG.pages.txt",
+    #"https://raw.githubusercontent.com/roosterkid/openproxylist/main/V2RAY_RAW.txt",
+    #"https://raw.githubusercontent.com/Rayan-Config/Rayan-Config.github.io/refs/heads/main/ALL",
+    "https://raw.githubusercontent.com/valid7996/Gozargah/refs/heads/main/Gozargah_Sub",
+    "https://raw.githubusercontent.com/Kwinshadow/TelegramV2rayCollector/main/sublinks/mix.txt",
+    "https://raw.githubusercontent.com/MhdiTaheri/V2rayCollector_Py/refs/heads/main/sub/Mix/mix.txt",
+    "https://raw.githubusercontent.com/Surfboardv2ray/Proxy-sorter/refs/heads/main/custom/mahsa.txt",
+    "https://raw.githubusercontent.com/mahsanet/MahsaFreeConfig/main/mci/sub_1.txt",
+    "https://raw.githubusercontent.com/mahsanet/MahsaFreeConfig/main/mci/sub_2.txt",
+    "https://raw.githubusercontent.com/mahsanet/MahsaFreeConfig/main/mci/sub_3.txt",
+    "https://raw.githubusercontent.com/mahsanet/MahsaFreeConfig/main/mci/sub_4.txt",
+    "https://raw.githubusercontent.com/mahsanet/MahsaFreeConfig/main/mtn/sub_1.txt",
+    "https://raw.githubusercontent.com/mahsanet/MahsaFreeConfig/main/mtn/sub_2.txt",
+    "https://raw.githubusercontent.com/mahsanet/MahsaFreeConfig/main/mtn/sub_3.txt",
+    "https://raw.githubusercontent.com/mahsanet/MahsaFreeConfig/main/mtn/sub_4.txt",
+    # A
     "https://t.me/s/ArV2ray",
+    # C
+    "https://t.me/s/ConfigsHUB2",
+    "https://t.me/s/configfa",
+    #"https://t.me/s/customv2ray",#149k other
+    # D
+    "https://t.me/s/DirectVPN",
+    "https://t.me/s/DailyV2RY",
+    # V
+    "https://t.me/s/v2rayfree",
+    "https://t.me/s/ViProxys",
+    "https://t.me/s/VlessConfig",
+    "https://t.me/s/v2ray_configs_pool",#vless
+    "https://t.me/s/v2ray_free_conf",
+    "https://t.me/s/vpnfail_vless",
+    "https://t.me/s/VmessProtocol",
+    "https://t.me/s/v2raybaz",
+    #"https://t.me/s/V2rayNGX",#vless
+    #"https://t.me/s/vpnowl",#vless
+    #"https://t.me/s/v2Source",#vless
+    #"https://t.me/s/VPN_Accounti",#vless ss 123K
+    #"https://t.me/s/V2rayNG3",#36k mix
+    #"https://t.me/s/vpnstorefast",#8k vless
+    #"https://t.me/s/Vpnstable",#1.8k vless
+    # F
+    "https://t.me/s/FreeV2rays",
+    #"https://t.me/s/flyv2ray",#37k mix
+    #"https://t.me/s/fastkanfig",#3k mix
+    # G
+    #"https://t.me/s/GetConfigIR",#vless
+    # P
+    "https://t.me/s/PrivateVPNs",
+    "https://t.me/s/pooriared",
+    "https://t.me/s/prrofile_purple",
+    #"https://t.me/s/Parsashonam",#tg vless ss
+    # I
+    #"https://t.me/s/ISVvpn",#tg vless ss
+    "https://t.me/s/IP_CF_Config",
+    #"https://t.me/s/ipV2Ray",#24k vless
+    #"https://t.me/s/lrnbymaa",#12k tg vless 
+    # L
+    "https://t.me/s/LonUp_M",
+    # M
+    "https://t.me/s/meli_proxyy",
+    #"https://t.me/s/melov2ray",#vless
+    #"https://t.me/s/mobilesignal", #tg vless ss
+    # S
+    "https://t.me/s/shokhmiplus",
+    "https://t.me/s/shadowproxy66",
+    #"https://t.me/s/SafeNet_Server",#20k tg vless ss
+    "https://t.me/s/moftconfig",
     "https://t.me/s/ZibaNabz",
     "https://t.me/s/nufilter",
+    # O
     "https://t.me/s/Outline_Vpn",
+    #"https://t.me/s/orange_vpns",#8k tg vless ss
     "https://t.me/s/zedmodeonVPN",
+    # R
+    "https://t.me/s/redv2ray_channel",
+    "https://t.me/s/redfree8",
+    # H
+    #"https://t.me/s/Hope_Net", # vmess ss other
+    # T
+    #"https://t.me/s/Trick_mobil",mix
     # other .
     #"https://raw.githubusercontent.com/10ium/ScrapeAndCategorize/refs/heads/main/output_configs/Vless.txt,
     #"https://t.me/s/WireVpnGuard",
@@ -41,7 +109,7 @@ USE_MAXIMUM_POWER = False
 
 # Desired number of configurations to fetch.
 # This is used only if USE_MAXIMUM_POWER is False.
-SPECIFIC_CONFIG_COUNT = 250
+SPECIFIC_CONFIG_COUNT = 500
 
 # Dictionary of protocols to enable or disable.
 # Set each protocol to True to enable, False to disable.
